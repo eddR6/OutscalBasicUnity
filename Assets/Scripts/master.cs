da72e8212d188c3e96f4c614c3cb385b5fe858b2
eddr6 <jainaditya400@gmail.com>
2020-06-30 18:22:15 +0530

    first commit

---
+++ Assets/Scripts/master.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class master : MonoBehaviour
+{
+    public Button btn;
+    public string sce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn.onClick.AddListener(ButtonClick);
+    }
+
+    void ButtonClick()
+    {
+        SceneManager.LoadScene(sce);
+    }
+}
